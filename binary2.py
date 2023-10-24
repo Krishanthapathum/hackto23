a460b133556874b4ad1629d634534f1881301052
Deshaninayanamini <87793827+Deshaninayanamini@users.noreply.github.com>
2023-10-25 00:37:53 +0530

    Create binary2.py

---
+++ binary2.py
@@ -0,0 +1,25 @@
+
+
+def _sum(arr):
+
+
+	sum = 0
+
+
+	for i in arr:
+		sum = sum + i
+
+	return(sum)
+
+
+# main function
+if __name__ == "__main__":
+	# input values to list
+	arr = [12, 3, 4, 15]
+
+	# calculating length of array
+	n = len(arr)
+	# calling function ans store the sum in ans
+	ans = _sum(arr)
+	# display sum
+	print('Sum of the array is ', ans)
